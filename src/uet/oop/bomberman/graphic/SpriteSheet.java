a2bd23556d0dd1667c679f401e8854ab0e705732
letthai <80798567+letthai@users.noreply.github.com>
2021-11-22 23:30:55 +0700

    update

--- src/uet/oop/bomberman/graphic/SpriteSheet.java
+++ src/uet/oop/bomberman/graphic/SpriteSheet.java
@@ -1,18 +1,41 @@
 package uet.oop.bomberman.graphic;
 
 
-public class SpriteSheet {
-    public String path;
-    public int Size;
-    public int[] pixels;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
-    public SpriteSheet(String s, int i) {
-        this.path = s;
-        this.Size = i;
-        pixels = new int[Size * Size];
+/**
+ * Tất cả sprite (hình ảnh game) được lưu trữ vào một ảnh duy nhất
+ * Class này giúp lấy ra các sprite riêng từ 1 ảnh chung duy nhất đó
+ * Link tham khảo: https://helpex.vn/question/lam-the-nao-de-trich-xuat-mot-phan-cua-hinh-anh-nay-trong-java-dong-cua-60ce1e67f137b0e523f87e63.
+ */
+
+public class SpriteSheet {
+    public String _path;
+    public final int SIZE;
+    public int[] _pixels;
+
+    public SpriteSheet(String path, int size) {
+        this._path = path;
+        this.SIZE = size;
+        _pixels = new int[size * size];
+        load();
     }
 
-    public static SpriteSheet Picture = new SpriteSheet("/textures/classic.png", 256);
+    public static SpriteSheet tiles = new SpriteSheet("/textures/classic.png", 256);
 
-
+    private void load() {
+        try {
+            URL a = SpriteSheet.class.getResource(_path);
+            BufferedImage image = ImageIO.read(a);
+            int w = image.getWidth();
+            int h = image.getHeight();
+            image.getRGB(0, 0, w, h, _pixels, 0, w);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
 }
